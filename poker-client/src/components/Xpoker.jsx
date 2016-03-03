3258013b7644c65658f2c16cd41958942ded501b
Sergio Tashdjian <sergio.tashdjian@x-team.com>
2016-03-03 09:11:12 -0300

    Bilerplate first commit

---
+++ poker-client/src/components/Xpoker.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default React.createClass({
+  render: function() {
+    return <div className="x-poker">
+      XPoker App Goes Here
+    </div>;
+  }
+});
---
+++ poker-client/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Xpoker from './components/Xpoker';
+
+
+
+ReactDOM.render(
+  <Xpoker />,
+  document.getElementById('x-poker-app')
+);
